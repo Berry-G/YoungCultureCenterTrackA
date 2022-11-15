d56321887914b4b50446c14246520b4f43758566
Shyeo2 <mhh4444@naver.com>
2022-11-15 17:23:59 +0900

    MemberService, MemberServiceImpl add signinMember

---
+++ YoungCultureCenter_TrackA/src/main/java/com/youngtvjobs/ycc/member/MemberService.java
@@ -0,0 +1,8 @@
+package com.youngtvjobs.ycc.member;
+
+
+
+public interface MemberService {
+	
+	void signinMember(MemberDto dto) throws Exception;
+}
---
+++ YoungCultureCenter_TrackA/src/main/java/com/youngtvjobs/ycc/member/MemberServiceImpl.java
@@ -0,0 +1,21 @@
+package com.youngtvjobs.ycc.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+	
+	
+	
+	@Autowired
+	MemberDao memberDao;
+	
+	@Override
+	public void signinMember(MemberDto dto) throws Exception {
+		memberDao.signinMember(dto);
+		
+		
+	}
+
+}

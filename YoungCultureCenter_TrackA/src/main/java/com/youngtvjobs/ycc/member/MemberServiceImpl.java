32f0616a0fd84a1034e6522f4248d4284cdba726
asel0joo <soojeon@nate.com>
2022-11-27 21:19:30 +0900

    method Redesigning

--- YoungCultureCenter_TrackA/src/main/java/com/youngtvjobs/ycc/member/MemberServiceImpl.java
+++ YoungCultureCenter_TrackA/src/main/java/com/youngtvjobs/ycc/member/MemberServiceImpl.java
@@ -1,5 +1,11 @@
 package com.youngtvjobs.ycc.member;
 
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -39,43 +45,77 @@ public class MemberServiceImpl implements MemberService{
 		return memberDao.update(memberDto);
 	}
 	
-	//이메일인증: mail_key값 생성하여 메일 발송해줌
+	//아이디 찾기
+	@Override
+	public String findId(HttpServletResponse response, String user_email) throws Exception {
+
+		String user_id = memberDao.findId(user_email);
+		
+		if (user_id == null) {
+			return null;
+		} else {
+			return user_id;
+		}
+	}
+	
+	//패스워드 찾기
+	@Override
+	public String findPw(HttpServletResponse response, String user_id) throws Exception {
+		String user_email = memberDao.findPw(user_id);
+		
+		if (user_email == null) {
+			return null;
+		} else {
+			return user_email;
+		}
+	}
+	
+	//이메일인증: mail_key값 생성하여 메일 발송세팅
 	@Override 
 	public String insertMember(String user_email) throws Exception {
 		
 			//랜덤 문자열을 생성해서 mail_key 컬럼에 넣어주기
 			String mail_key = new TempKey().getKey(7, false);	//랜덤키 길이 설정
 			
-			//회원가입 완료하면 인증을 위한 이메일 발송
-			MailHandler sendMail = new MailHandler(mailSender);
-			sendMail.setSubject("[Young문화체육센터 인증메일 입니다.]"); 	//메일제목
-			sendMail.setText(
-					"<h1>Young문화체육센터 메일인증</h1>" +
+			String mail_title = "[Young문화체육센터 인증메일 입니다.]";
+			String mail_text = "<h1>Young문화체육센터 메일인증</h1>" +
 					"<br>Young문화체육센터에 오신것을 환영합니다!" +
 					"<br>아래 인증번호를 인증번호 입력창에 입력해주세요." +
-					"<p><b>인증번호: "+ mail_key +"</b></p>");
-			sendMail.setFrom("soojeontest01@gmail.com", "Young문화체육센터");
-			System.out.println(user_email);
-			sendMail.setTo(user_email);
-			sendMail.send();
+					"<p><b>인증번호: "+ mail_key +"</b></p>";
+			
+			this.snedEmail(user_email, mail_title, mail_text);
+			
 			
 			return mail_key;
 		}
-	@Override
-	public int updateMailKey(MemberDto memberDto) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	//패스워드 찾을때 이메일 발송 세팅
+	public String pwSendEmail(String user_email) throws Exception {
+		
+		//랜덤 문자열을 생성해서 mail_key 컬럼에 넣어주기
+		String pw = memberDao.findPword(user_email);
+		
+		String email_title = "[Young문화체육센터 비밀번호 입니다.]";
+		String email_text = "<h1>Young문화체육센터 비밀번호 찾기</h1>" +
+				"<p><b>비밀 번호 : "+ pw +"</b></p>";
+		
+		this.snedEmail(user_email, email_title, email_text);
+		
+		return user_email;
+		
 	}
-	@Override
-	public int updateMailAuth(MemberDto memberDto) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	//이메일 발송 함수
+	public void snedEmail(String user_email, String email_title, String email_text) throws Exception {
+		//회원가입 완료하면 인증을 위한 이메일 발송
+		MailHandler sendMail = new MailHandler(mailSender);
+		sendMail.setSubject(email_title); 	//메일제목
+		sendMail.setText(email_text);
+		sendMail.setFrom("soojeontest01@gmail.com", "Young문화체육센터");
+		sendMail.setTo(user_email);
+		sendMail.send();
 	}
-	@Override
-	public int emailAuthFail(String id) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+
 
 	
 }

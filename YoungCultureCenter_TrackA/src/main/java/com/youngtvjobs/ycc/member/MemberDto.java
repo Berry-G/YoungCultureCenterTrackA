789ce8b2b18b35d57cc4917bc95b6f88e8a50ab0
asel0joo <soojeon@nate.com>
2022-12-29 17:17:41 +0900

    change anotation

--- YoungCultureCenter_TrackA/src/main/java/com/youngtvjobs/ycc/member/MemberDto.java
+++ YoungCultureCenter_TrackA/src/main/java/com/youngtvjobs/ycc/member/MemberDto.java
@@ -6,14 +6,17 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Data
+@Getter
+@Setter
 public class MemberDto
 {
 
